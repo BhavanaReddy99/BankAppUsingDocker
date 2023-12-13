c5cd42f779b367a0150f32b1177b37a96f357e70
BhavanaReddy99 <jorrigalabhavana01@gmail.com>
2023-12-13 18:27:45 +0530

    Add project files.

---
+++ BankAppUsingDocker/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Threading.Tasks;
+
+namespace BankAppUsingDocker.Pages
+{
+    public class DashboardModel : PageModel
+    {
+        public async Task<IActionResult> OnPostLogoutAsync()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/Login");
+        }
+
+    }
+}
---
+++ BankAppUsingDocker/Pages/TransferSucess.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankAppUsingDocker.Pages
+{
+    public class TransferSucessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
